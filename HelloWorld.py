b7b6bc572bcf04155ef548d4b3cfbb17f91e6272
rahaf20-meet <rahaf20@meet.mit.edu>
2018-07-31 07:30:38 -0400

    Stopping here

---
+++ HelloWorld.py
@@ -0,0 +1,3 @@
+language = "English"
+translation = "Hello World"
+print("The translation of hello world in " + language + " is " + translation)
---
+++ greeting.py
@@ -0,0 +1,8 @@
+name = input("what's your name?")
+name = name.capitalize()
+print("Hello there " + name)
+lenght = len(name)
+print("Your name is" + length + "letters long!")
+      
+
+
